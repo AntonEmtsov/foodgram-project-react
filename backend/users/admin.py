3f2f34b4f4865bd5a74a56602f7771f21c4db38c
russ044 <russ044@yandex.ru>
2022-11-26 13:18:26 +0400

    0.0.3

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'role', 'username', 'email',)
+    search_fields = ('username', 'role',)
+    list_filter = ('role', 'is_superuser',)
+    empty_value_display = '-пусто-'
