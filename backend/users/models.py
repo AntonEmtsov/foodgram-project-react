6a6634374c1928becbc9deb11375087aa06a60e1
russ044 <russ044@yandex.ru>
2022-11-26 12:55:56 +0400

    0.0.2 /added app "Users"/added models.py/User

---
+++ backend/users/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from foodgram import settings
+
+USER = 'user'
+MODERATOR = 'moderator'
+ADMINISTRATOR = 'admin'
+ROLES = [
+    (USER, 'Пользователь'),
+    (MODERATOR, 'Модератор'),
+    (ADMINISTRATOR, 'Администратор'),
+]
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=settings.EMAIL_MAX_LENGTH,
+        unique=True,
+    )
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=settings.USERNAME_MAX_LENGTH,
+        unique=True,
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=max(len(role) for role, _ in ROLES),
+        choices=ROLES,
+        default=USER,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=settings.FIRST_NAME_MAX_LENGTH,
+        blank=True,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=settings.LAST_NAME_MAX_LENGTH,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
