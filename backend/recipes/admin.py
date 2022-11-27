c15f3a252f8bc60c899bc16b2f04f662638cf5f1
russ044 <russ044@yandex.ru>
2022-11-27 15:51:59 +0400

    0.1.0

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Ingredient,
+    Tag,
+    Recipe,
+    UserFavoriteRecipes,
+    ShoppingList,
+)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'name')
+    search_fields = ('author', 'name', 'tags')
+    empty_value_display = '-пусто-'
+
+
+
+
+@admin.register(UserFavoriteRecipes)
+class UserFavoriteRecipesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
