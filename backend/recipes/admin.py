fe0304d2610bc13e593390a5039bcf3281c856d8
russ044 <russ044@yandex.ru>
2022-12-04 20:16:02 +0400

    0.3.7

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 
-from .models import (
-    Ingredient,
-    Recipe,
-    ShoppingList,
-    Tag,
-    UserFavoriteRecipes,
-    IngredientsInRecipe
-)
+from .models import (Ingredient, IngredientsInRecipe, Recipe, ShoppingList,
+                     Tag, UserFavoriteRecipes)
 
 
 class IngredientRecipeInline(admin.TabularInline):
