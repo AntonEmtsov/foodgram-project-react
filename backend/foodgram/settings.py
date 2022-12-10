2abb489a9ec16d98a7bef6f4253d7ace75329f81
russ044 <russ044@yandex.ru>
2022-12-10 15:09:27 +0400

    2.3.7

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 DEBUG = os.getenv('DEBUG', default=False)
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=('localhost', 'backend')).split(' ')  # noqa
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*']).split(' ')  # noqa
 # ALLOWED_HOSTS = ('localhost', 'backend')
 
 INSTALLED_APPS = [
